10d2116de02a3780d408e4ff196878ddf9a916f9
shoham.elad <shoham.elad@gmail.com>
2024-02-21 01:10:24 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crohn_recommendation',
+    version='1.0.0',
+    packages=find_packages(),
+    description='recommendation system for crohn disease',
+    author='Elad Shoham',
+    author_email='shellad@post.bgu.ac.il',
+    url='https://github.com/BGU-Elad/crohn',
+    classifiers=[
+        'Programming Language :: Python :: 3.11.5',
+    ],
+)
