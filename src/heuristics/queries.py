d2edb986dc3f66546cc0ad37064409aa39bca380
shoham.elad <40741297+x-lad@users.noreply.github.com>
2025-07-23 23:58:17 +0300

    fix: skip first exercise

--- src/heuristics/queries.py
+++ src/heuristics/queries.py
@@ -176,7 +176,8 @@ LAST_TIME_MESSAGE_QUERY = """
 
 TRENDS_QUERY = """
     WITH 
-    last_x_mesurements AS (SELECT * FROM Exercise WHERE userId = {user} ORDER BY dateStart DESC LIMIT {measurements})
+    last_x_mesurements AS (SELECT * FROM Exercise WHERE userId = {user} AND dateStart != (SELECT MIN(dateStart) FROM Exercise WHERE userId = {user})
+     ORDER BY dateStart DESC LIMIT {measurements})
     SELECT q1.suds_stress AS sudsQ1, q1.fatigue AS fatigueQ1, q1.vas_pain AS vasQ1, q2.suds_stress AS sudsQ2,
     q2.fatigue AS fatigueQ2, q2.vas_pain AS vasQ2
     FROM last_x_mesurements as l JOIN Questionnaire as q1 on l.questionnairePrimerId = q1.questionnaireId
